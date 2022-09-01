7d70a555ede81bf8b7845de4e2f01611a0e704b4
Anolue Ekene Samuel <kenelight4u@gmail.com>
2022-09-01 02:19:31 +0100

    Set Up App in progress

---
+++ LibraryManagement/LM.Domain/Entities/LMUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LM.Domain.Entities
+{
+    public class LMUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime RegisteredDate { get; set; }
+        public DateTime PasswordResetDate { get; set; }
+        public string OTP { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
