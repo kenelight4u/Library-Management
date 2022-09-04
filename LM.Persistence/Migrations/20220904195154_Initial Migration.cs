15cf36d962605e79e8ee7036c2b04e69a4d6a86e
Anolue Ekene Samuel <kenelight4u@gmail.com>
2022-09-04 20:54:31 +0100

    Seeded Data complete

--- LM.Persistence/Migrations/20220904195154_Initial Migration.cs
+++ LM.Persistence/Migrations/20220904195154_Initial Migration.cs
@@ -174,11 +174,11 @@ namespace LM.Persistence.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "Books",
+                name: "Book",
                 columns: table => new
                 {
                     ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ISBN = table.Column<string>(type: "nvarchar(450)", nullable: true),
                     Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Author = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
@@ -190,9 +190,9 @@ namespace LM.Persistence.Migrations
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Books", x => x.ID);
+                    table.PrimaryKey("PK_Book", x => x.ID);
                     table.ForeignKey(
-                        name: "FK_Books_BookGenres_BookGenresId",
+                        name: "FK_Book_BookGenres_BookGenresId",
                         column: x => x.BookGenresId,
                         principalTable: "BookGenres",
                         principalColumn: "ID",
@@ -223,15 +223,15 @@ namespace LM.Persistence.Migrations
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Restrict);
                     table.ForeignKey(
-                        name: "FK_BookHistories_Books_BookId",
+                        name: "FK_BookHistories_Book_BookId",
                         column: x => x.BookId,
-                        principalTable: "Books",
+                        principalTable: "Book",
                         principalColumn: "ID",
                         onDelete: ReferentialAction.Restrict);
                 });
 
             migrationBuilder.CreateTable(
-                name: "BookInventories",
+                name: "BookInventory",
                 columns: table => new
                 {
                     ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
@@ -243,14 +243,59 @@ namespace LM.Persistence.Migrations
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_BookInventories", x => x.ID);
+                    table.PrimaryKey("PK_BookInventory", x => x.ID);
                     table.ForeignKey(
-                        name: "FK_BookInventories_Books_BookId",
+                        name: "FK_BookInventory_Book_BookId",
                         column: x => x.BookId,
-                        principalTable: "Books",
+                        principalTable: "Book",
                         principalColumn: "ID",
                         onDelete: ReferentialAction.Restrict);
                 });
+
+            migrationBuilder.InsertData(
+                table: "BookGenres",
+                columns: new[] { "ID", "CreationTime", "CreatorUserId", "Description", "LastModificationTime", "Name" },
+                values: new object[] { new Guid("d6b44d21-d94b-4716-acaa-f26a6328fa0b"), new DateTime(2022, 9, 4, 20, 51, 51, 623, DateTimeKind.Local).AddTicks(490), "473c9df0-2634-4506-ad6c-9b98336082f2", "These books are based in a time period set in the past decades, often against the backdrop of significant (real) historical events.", null, "Historical Fiction" });
+
+            migrationBuilder.InsertData(
+                table: "BookGenres",
+                columns: new[] { "ID", "CreationTime", "CreatorUserId", "Description", "LastModificationTime", "Name" },
+                values: new object[] { new Guid("8bea3613-18a2-4bd6-a8a3-6e34ce4881f3"), new DateTime(2022, 9, 4, 20, 51, 51, 623, DateTimeKind.Local).AddTicks(4360), "473c9df0-2634-4506-ad6c-9b98336082f2", "The plot always revolves around a crime of sorts that must be solved—or foiled—by the protagonists.", null, "Detective and Mystery" });
+
+            migrationBuilder.InsertData(
+                table: "BookGenres",
+                columns: new[] { "ID", "CreationTime", "CreatorUserId", "Description", "LastModificationTime", "Name" },
+                values: new object[] { new Guid("f07f8895-b747-459d-ceb1-85f77c5e7d67"), new DateTime(2022, 9, 4, 20, 51, 51, 623, DateTimeKind.Local).AddTicks(4378), "473c9df0-2634-4506-ad6c-9b98336082f2", "The stories in comic books and graphic novels are presented to the reader through engaging, sequential narrative art (illustrations and typography) that's either presented in a specific design or the traditional panel layout you find in comics. With both, you'll often find the dialogue presented in the tell-tale \"word balloons\" next to the respective characters.", null, "Comic Book or Graphic Novel" });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "ID", "Author", "BookGenresId", "CreationTime", "CreatorUserId", "Description", "ISBN", "LastModificationTime", "Quantity", "Title" },
+                values: new object[] { new Guid("5ee58416-76d4-4bad-852e-dc2ed486e482"), " Gabriel Garcia Marquez ", new Guid("d6b44d21-d94b-4716-acaa-f26a6328fa0b"), new DateTime(2022, 9, 4, 20, 51, 51, 625, DateTimeKind.Local).AddTicks(9076), "473c9df0-2634-4506-ad6c-9b98336082f2", "One Hundred Years of Solitude is the first piece of literature since the Book of Genesis that should be required reading for the entire human race. . . . Mr. Garcia Marquez has done nothing less than to create in the reader a sense of all that is profound, meaningful, and meaningless in life.", "978-3-16-148410-0", null, 4, "One Hundred Years of Solitude" });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "ID", "Author", "BookGenresId", "CreationTime", "CreatorUserId", "Description", "ISBN", "LastModificationTime", "Quantity", "Title" },
+                values: new object[] { new Guid("7969a9ac-c081-4b75-a7fc-51e64c79c8dc"), "Alan Moore", new Guid("8bea3613-18a2-4bd6-a8a3-6e34ce4881f3"), new DateTime(2022, 9, 4, 20, 51, 51, 625, DateTimeKind.Local).AddTicks(9113), "473c9df0-2634-4506-ad6c-9b98336082f2", "A hit HBO original series, Watchmen, the groundbreaking series from award-winning author Alan Moore, presents a world where the mere presence of American superheroes changed history--the U.S. won the Vietnam War, Nixon is still president, and the Cold War is in full effect.", "978-3-16-148410-1", null, 2, "Watchmen " });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "ID", "Author", "BookGenresId", "CreationTime", "CreatorUserId", "Description", "ISBN", "LastModificationTime", "Quantity", "Title" },
+                values: new object[] { new Guid("13088fc2-c0ee-487e-91a1-2b977214d0a1"), "Alan Moore", new Guid("f07f8895-b747-459d-ceb1-85f77c5e7d67"), new DateTime(2022, 9, 4, 20, 51, 51, 625, DateTimeKind.Local).AddTicks(9117), "473c9df0-2634-4506-ad6c-9b98336082f2", "After the sinking of a cargo ship, a solitary lifeboat remains bobbing on the wild blue Pacific. The only survivors from the wreck are a sixteen-year-old boy named Pi, a hyena, a wounded zebra, an orangutan—and a 450-pound Royal Bengal tiger.\r\n\r\nSoon the tiger has dispatched all but Pi Patel, whose fear, knowledge, and cunning allow him to coexist with the tiger, Richard Parker, for 227 days while lost at sea. When they finally reach the coast of Mexico, Richard Parker flees to the jungle, never to be seen again. The Japanese authorities who interrogate Pi refuse to believe his story and press him to tell them \"the truth.\" After hours of coercion, Pi tells a second story, a story much less fantastical, much more conventional—but is it more true?\r\n\r\n\"A story to make you believe in the soul-sustaining power of fiction.\"—Los Angeles Times Book Review\r\n", "978-3-16-148410-2", null, 3, "Life Of Pi Paperback" });
+
+            migrationBuilder.InsertData(
+                table: "BookInventory",
+                columns: new[] { "ID", "BookId", "CreationTime", "CreatorUserId", "LastModificationTime", "Quantity" },
+                values: new object[] { new Guid("8ec05cfa-11e5-4f32-b732-dbd3a953b20b"), new Guid("5ee58416-76d4-4bad-852e-dc2ed486e482"), new DateTime(2022, 9, 4, 20, 51, 51, 624, DateTimeKind.Local).AddTicks(8661), "473c9df0-2634-4506-ad6c-9b98336082f2", null, 4 });
+
+            migrationBuilder.InsertData(
+                table: "BookInventory",
+                columns: new[] { "ID", "BookId", "CreationTime", "CreatorUserId", "LastModificationTime", "Quantity" },
+                values: new object[] { new Guid("536a1a94-c132-4163-bee5-2d0e1986882a"), new Guid("7969a9ac-c081-4b75-a7fc-51e64c79c8dc"), new DateTime(2022, 9, 4, 20, 51, 51, 624, DateTimeKind.Local).AddTicks(8698), "473c9df0-2634-4506-ad6c-9b98336082f2", null, 2 });
+
+            migrationBuilder.InsertData(
+                table: "BookInventory",
+                columns: new[] { "ID", "BookId", "CreationTime", "CreatorUserId", "LastModificationTime", "Quantity" },
+                values: new object[] { new Guid("13b5ce77-b382-45b6-bff4-300bb8b93388"), new Guid("13088fc2-c0ee-487e-91a1-2b977214d0a1"), new DateTime(2022, 9, 4, 20, 51, 51, 624, DateTimeKind.Local).AddTicks(8702), "473c9df0-2634-4506-ad6c-9b98336082f2", null, 3 });
 
             migrationBuilder.CreateIndex(
                 name: "IX_AspNetRoleClaims_RoleId",
@@ -292,6 +337,18 @@ namespace LM.Persistence.Migrations
                 filter: "[NormalizedUserName] IS NOT NULL");
 
             migrationBuilder.CreateIndex(
+                name: "IX_Book_BookGenresId",
+                table: "Book",
+                column: "BookGenresId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_ISBN",
+                table: "Book",
+                column: "ISBN",
+                unique: true,
+                filter: "[ISBN] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
                 name: "IX_BookHistories_BookId",
                 table: "BookHistories",
                 column: "BookId");
@@ -302,14 +359,9 @@ namespace LM.Persistence.Migrations
                 column: "LMUserId");
 
             migrationBuilder.CreateIndex(
-                name: "IX_BookInventories_BookId",
-                table: "BookInventories",
+                name: "IX_BookInventory_BookId",
+                table: "BookInventory",
                 column: "BookId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Books_BookGenresId",
-                table: "Books",
-                column: "BookGenresId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -333,7 +385,7 @@ namespace LM.Persistence.Migrations
                 name: "BookHistories");
 
             migrationBuilder.DropTable(
-                name: "BookInventories");
+                name: "BookInventory");
 
             migrationBuilder.DropTable(
                 name: "AspNetRoles");
@@ -342,7 +394,7 @@ namespace LM.Persistence.Migrations
                 name: "AspNetUsers");
 
             migrationBuilder.DropTable(
-                name: "Books");
+                name: "Book");
 
             migrationBuilder.DropTable(
                 name: "BookGenres");
