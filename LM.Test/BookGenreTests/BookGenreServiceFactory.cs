5e3c8bcb9537ab17f93490f178dd1556be4259ac
Anolue Ekene Samuel <kenelight4u@gmail.com>
2022-09-05 12:24:14 +0100

    SetUp for Service Class Done

---
+++ LM.Test/BookGenreTests/BookGenreServiceFactory.cs
@@ -0,0 +1,34 @@
+﻿using LM.Application.Interfaces.Persistence;
+using LM.Application.Interfaces.Utilities;
+using LM.Domain.Common;
+using LM.Domain.Entities;
+using LM.Services.Implementations;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LM.Test.BookGenreTests
+{
+    public class BookGenreServiceFactory
+    {
+        public Mock<IStoreManager<BookGenres>> BookGenreRepository = new Mock<IStoreManager<BookGenres>>();
+        public Mock<IUnitOfWork> UnitOfWork = new Mock<IUnitOfWork>();
+        public Mock<IContextAccessor> ContextAccessor = new Mock<IContextAccessor>();
+
+        public BookGenreServiceFactory()
+        {
+            BookGenreService = new BookGenreService(
+                BookGenreRepository.Object,
+                ContextAccessor.Object,
+                UnitOfWork.Object
+                );
+
+            ContextAccessor.Setup(x => x.GetCurrentUserId()).Returns(new UserPrincipal(TestData.GetAuthenticatedUser()).UserId);
+        }
+
+        public BookGenreService BookGenreService { get; set; }
+    }
+}
---
+++ LM.Test/BookTests/AddNewBookTests.cs
@@ -0,0 +1,48 @@
+﻿using LM.Domain.Entities;
+using LM.DTOs.Request.BookDTO;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LM.Test.BookTests
+{
+    public class AddNewBookTests
+    {
+        private readonly BookServiceFactory _fac;
+
+        public AddNewBookTests()
+        {
+            _fac = new BookServiceFactory();
+        }
+
+        [Fact]
+        public void AddNewBook_ShouldReturnError_WhenBookGenreIdIsInvalid()
+        {
+            // Arrange
+            Guid invalidId = Guid.Parse("b062469f-3d58-48b8-8626-4a7a629d3831");
+
+            var model = new BookDTO { Author = "Ekene Test", BookGenresId = invalidId };
+            var bookGenres = TestData.GetBookGenres();
+            var books = TestData.GetBooks();
+
+            _fac.BookGenreRepository.Setup(x => x.DataStore.GetAllQuery())
+            .Equals(bookGenres.FirstOrDefault(x => x.ID == invalidId));
+
+            //Act
+            var result = _fac.BookService.AddNewBook(model);
+
+            //Assert
+            _fac.BookGenreRepository.Verify(x => x.DataStore.GetAllQuery(), Times.Once);
+            _fac.UnitOfWork.Verify(x => x.SaveChangesAsync(), Times.Never);
+
+            Assert.NotNull(result);
+        }
+    }
+}
